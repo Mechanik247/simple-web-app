17d0a7d50e3c38568faab2b57f39715764981817
mechanik247 <efimov.vladislav2011@yandex.ru>
2019-01-12 14:56:07 +0400

    This is MY CODE!

--- src/Servlet.java
+++ src/Servlet.java
@@ -22,11 +22,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
-/**
- * This servlet class demonstrates how to access a JNDI DataSource that
- * represents a JDBC connection.
- * @author www.codejava.net
- */
 public class Servlet extends HttpServlet {
 
     public void init(ServletConfig config) throws ServletException {
